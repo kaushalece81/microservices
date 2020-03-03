3a5842b1502d5acba0556b6e57bc9e406d9f203a
kaushalece81 <837305+kaushalece81@users.noreply.github.com>
2020-03-03 17:54:08 +0530

    added dev qa properties

--- spring-cloud-config-server/src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,12 +2,17 @@ package com.in28minutes.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
+//@EnableAutoConfiguration
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+		String abc="file:///C:/GitRepoMicroservices/microservices/git-localconfig-repo";
+		System.out.println("====="+abc);
 	}
 
 }
